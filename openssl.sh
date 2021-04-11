b1399e4a6f6ffd89e7e4cc59cfdae5fcdca2c65a
SquadraSec <pboerman2897@outlook.com>
2021-04-11 21:23:31 +0200

    Create openssl.sh

---
+++ openssl.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "[req]
+default_bits  = 2048
+distinguished_name = req_distinguished_name
+req_extensions = req_ext
+x509_extensions = v3_req
+prompt = no
+
+[req_distinguished_name]
+countryName = Nextcloud
+stateOrProvinceName = Nextcloud
+localityName = Nextcloud
+organizationName = Nextcloud
+commonName = $IP: Nextcloud
+
+[req_ext]
+subjectAltName = @alt_names
+
+[v3_req]
+subjectAltName = @alt_names
+
+[alt_names]
+IP.1 = $IP
+IP.2 = $EXT_IP
+" > san.cnf
+
+openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout key.pem -out cert.pem -config san.cnf
+rm san.cnf
